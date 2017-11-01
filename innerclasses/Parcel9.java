74ab2c640e158cff0071ef33728cf0738d10a867
zj <zhoujingnever@163.com>
2017-11-01 18:08:59 +0800

    first commit

---
+++ innerclasses/Parcel9.java
@@ -0,0 +1,20 @@
+//: innerclasses/Parcel9.java
+// An anonymous inner class that performs
+// initialization. A briefer version of Parcel5.java.
+
+public class Parcel9 {
+  // Argument must be final to use inside
+  // anonymous inner class:
+  // jdk 1.8就没有这个限制了
+  // Destination是一个接口 在这里可以直接创建对象
+  public Destination destination(final String dest) {
+    return new Destination() {
+      private String label = dest;
+      public String readLabel() { return label; }
+    };
+  }
+  public static void main(String[] args) {
+    Parcel9 p = new Parcel9();
+    Destination d = p.destination("Tasmania");
+  }
+} ///:~
---
+++ innerclasses/Sequence.java
@@ -0,0 +1,39 @@
+//: innerclasses/Sequence.java
+// Holds a sequence of Objects.
+
+//  迭代器设计模式，在接口中定义好相关方法，子类按照的自己的迭代方式进行实现
+interface Selector {
+  boolean end();
+  Object current();
+  void next();
+}	
+
+public class Sequence {
+  private Object[] items;
+  private int next = 0;
+  public Sequence(int size) { items = new Object[size]; }
+  public void add(Object x) {
+    if(next < items.length)
+      items[next++] = x;
+  }
+  private class SequenceSelector implements Selector {
+    private int i = 0;
+	// 内部类直接使用外部类的私用属性items
+    public boolean end() { return i == items.length; }
+    public Object current() { return items[i]; }
+    public void next() { if(i < items.length) i++; }
+  }
+  public Selector selector() {
+    return new SequenceSelector();
+  }	
+  public static void main(String[] args) {
+    Sequence sequence = new Sequence(10);
+    for(int i = 0; i < 10; i++)
+      sequence.add(Integer.toString(i));
+    Selector selector = sequence.selector(); 
+    while(!selector.end()) {
+      System.out.print(selector.current() + " ");
+      selector.next();
+    }
+  }
+} 
